1a9988f6e43b4fa45c8aaa5aca306157788af2ed
Frensis7153 <bafuinsoni@yandex.ru>
2021-05-13 09:47:50 +0300

    Удалил из мастера все файлы

--- 2_1.py
+++
@@ -1,4 +0,0 @@
-my_list = [34, 45., True, None, (0, ), [2], dict(), set()]
-
-for el in my_list:
-    print(f'{el} - {type(el)}')
--- 2_4.py
+++
@@ -1,3 +0,0 @@
-words = input("Введите строку: ").split()
-for ind, word in enumerate(words, 1):
-    print(ind, word[:10])
